aa1b10e065c2f7466458721de01ef7f5f0860f04
pyzhov.egor <142977368+nxf1ve@users.noreply.github.com>
2024-10-18 16:36:44 +0400

    create project

---
+++ src/main/java/ru/pyzhov/UserControlAPI/UserControlApiApplication.java
@@ -0,0 +1,13 @@
+package ru.pyzhov.UserControlAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pyzhov/UserControlAPI/UserControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pyzhov.UserControlAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
